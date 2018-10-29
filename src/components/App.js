c56e17e1d9c604d2320667e8929487bf29cccc83
ahdeshpande <deshpande.abhiraj@gmail.com>
2018-10-28 23:18:33 -0400

    feat: Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <h1>Would you rather?</h1>
+        );
+    }
+}
+
+export default App;

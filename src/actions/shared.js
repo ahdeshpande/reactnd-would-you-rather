6b7d0fb3d37cddaefd4b13ac9a5a312ccb8b55e9
ahdeshpande <deshpande.abhiraj@gmail.com>
2018-11-12 22:12:26 -0500

    feat: Redux - Users

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,6 +1,7 @@
 import {hideLoading, showLoading} from "react-redux-loading";
 import {getInitialData} from "../utils/api";
 import {getQuestions} from "./questions";
+import {getUsers} from "./users";
 // import {setAuthedUser} from "./authedUser";
 
 // const AUTHED_ID = 'ahdeshpande';
@@ -11,6 +12,7 @@ export function handleInitialData() {
         return getInitialData()
             .then(({users, questions}) => {
                 dispatch(getQuestions(questions));
+                dispatch(getUsers(questions));
                 // dispatch(setAuthedUser(AUTHED_ID));
                 dispatch(hideLoading);
             });

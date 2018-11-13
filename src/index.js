9deee3648f26617f1c1e8aab707492e8cebb7bf3
ahdeshpande <deshpande.abhiraj@gmail.com>
2018-11-12 22:19:08 -0500

    feat: Create store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from "react-router-dom";
+import {createStore} from "redux";
+import {Provider} from 'react-redux';
+import reducers from './reducers';
+import middleware from './middleware';
 import './index.css';
 import App from './components/App';
 
+const store = createStore(reducers, middleware);
+
 ReactDOM.render(
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>,
     document.getElementById('root'));

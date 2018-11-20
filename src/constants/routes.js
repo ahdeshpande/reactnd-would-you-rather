f69d6c1c3e772fafe819cd4ed78f48a0154dccd9
ahdeshpande <deshpande.abhiraj@gmail.com>
2018-11-19 23:08:05 -0500

    feat: Update routes

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,6 +1,6 @@
 export const SIGN_UP = '/signup';
 export const LOGIN = '/login';
 export const LANDING = '/';
-export const DASHBOARD = '/dashboard';
-export const NEW_QUESTION = '/new_question';
+export const DASHBOARD = '/';
+export const NEW_QUESTION = '/add';
 export const LEADERBOARD = '/leaderboard';
